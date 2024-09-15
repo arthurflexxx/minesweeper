68f6292566f2ba5648222a9398034a07d2ffe5eb
arthurflexxx <mr.sasha.mikhaylov.04@mail.ru>
2024-09-15 16:56:55 +0500

    fixes

--- main.py
+++ main.py
@@ -15,15 +15,12 @@ def input_game_size():
 
 
 def input_cell_coord(game: Minesweeper):
-    # Get user input for the row and column to interact with
-    try:
+    while True:
+        # Get user input for the row and column of the cell to interact with
         row, col = map(int, input("row, col: ").split())
-        assert 0 <= row < game.board.rows and 0 <= col < game.board.cols
-    except (ValueError, AssertionError):
-        print("f")
-
-    f = input("Flag (Print anything if you want to put a flag): ")
-
+        if 0 <= row < game.board.rows and 0 <= col < game.board.cols:
+            f = input("Flag (Print anything if you want to put a flag): ")
+            break
     return row, col, f
 
 
@@ -31,20 +28,20 @@ def play():
     """
     Main function to start and run the Minesweeper game.
     """
-    # rows, cols, mines = input_game_size()
+    rows, cols, mines = input_game_size()
 
     # Create a Minesweeper game instance with the specified parameters
-    game: Minesweeper = Minesweeper(5, 5, 1)
+    game: Minesweeper = Minesweeper(rows, cols, mines)
 
     while not game.is_game_over:
         # Clear the console and display remaining bombs
-        # print("\033[H\033[J", end="")
+        print("\033[H\033[J", end="")
         print(f"Bombs left: {game.bombs_left}")
         game.board.draw_board()
 
         row, col, f = input_cell_coord(game)
-
-        game.game_cycle(row, col, f)
+        a = game.game_cycle(row, col, f)
+        game.show_end_game_stats(a)
 
 
 if __name__ == "__main__":
--- minesweeper_module/minesweeper.py
+++ minesweeper_module/minesweeper.py
@@ -52,22 +52,29 @@ class Minesweeper:
                             queue.append((i, j))
 
     def game_cycle(self, row, col, put_flag=False):
+        """
+        Handles a single game cycle, processing player's move and updating game state.
+
+        Args:
+            row (int): y coord
+            col (int): x coord
+            put_flag (bool, optional): Whether to place a flag on the cell. Defaults to False.
+
+        Returns:
+            None. Updates the game state directly.
+        """
         if self.first_move:
             self.make_first_move(row, col)
-            self.check_win()
-            return
+            return self.check_win()
 
         if put_flag:
             self.toggle_flag(row, col)
         else:
             if self.check_step_on_mine(row, col):
-                self.game_end()
-                return
+                return self.game_end()
             self.reveal(row, col)
 
-        if self.check_win():
-            self.game_end(win=True)
-            return
+        return self.check_win()
 
     def toggle_flag(self, row, col):
         self.board.get_cell(row, col).toggle_flag()
@@ -77,33 +84,35 @@ class Minesweeper:
             self.bombs_left -= 1
         else:
             # Remove the flag if it's already flagged
-            self.bombs_left += 1
             self.board.flags.remove((row, col))
+            self.bombs_left += 1
 
     def check_step_on_mine(self, row, col):
         return (row, col) in self.board.mines  # Return True if the cell is a mine
 
     def check_win(self):
-        return (
-            self.board.flags == self.board.mines and self.board.mines
-        ) or self.board.rows * self.board.cols == len(self.visited) + len(
+        if self.board.flags == self.board.mines and self.board.mines:
+            return self.game_end(win=True, win_by_flags=True)
+        elif self.board.rows * self.board.cols == len(self.visited) + len(
             self.board.mines
-        )  # Return True if all mines are flagged or all non-mine cells are revealed
+        ):
+            return self.game_end(win=True)
 
-    def game_end(self, win=False, win_by_flags=False, show_mines=True):
+    def show_end_game_stats(self, game_status):
+        # print("\033[H\033[J", end="")  # Clear the console
+        print(game_status)
+        print(f"Play time: {self.play_time} seconds\n")
+        self.board.draw_board()
+
+    def game_end(self, win=False, win_by_flags=False):
         """
         Ends the game, displaying the results and the board.
         """
-        print("\033[H\033[J", end="")  # Clear the console
         self.is_game_over = True
+        self.play_time = int(time.time() - self.start_time) // 1  # Calculate play time
 
         # Show mines if the player lost or won but not by flagging all mines
-        if not win_by_flags and show_mines:
+        if not win_by_flags:
             self.board.show_mines()
-
-        self.play_time = int(time.time() - self.start_time) // 1  # Calculate play time
-        print("Win" if win else "Lose")
-        print(f"Play time: {self.play_time} seconds\n")
-        self.board.draw_board()
 
         return "Win" if win else "Lose"
